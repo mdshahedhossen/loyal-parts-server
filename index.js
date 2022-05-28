ac91b269accaeaf0a545b1599a7b82e94360d304
mdshaedhossen <shaheddiu23@gmail.com>
2022-05-28 21:00:03 +0600

    payment commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 var jwt = require('jsonwebtoken');
 require('dotenv').config()
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -52,13 +52,15 @@ async function run(){
 
         app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
           const service = req.body;
-          const price = service.price;
+          const price = service.totalAmount;
+          console.log(price)
           const amount = price*100;
           const paymentIntent = await stripe.paymentIntents.create({
             amount : amount,
             currency: 'usd',
             payment_method_types:['card']
           });
+          
           res.send({clientSecret: paymentIntent.client_secret})
         });
 
